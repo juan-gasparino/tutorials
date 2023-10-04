774f1f8b5968aeaca230d78bc352b15ba569ffd7
Juan Gasparino <juan.gasparino@gmail.com>
2023-10-04 13:44:30 +0200

    push pagination example

---
+++ react/pagination/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+import Search from './Search'
+
+const App = () => {
+	return (
+		<div className="App">
+			<h1>Movies API</h1>
+			<Search />
+		</div>
+	)
+}
+
+export default App
