7c669778c6792bb8f6a690a134490320a64ce674
Juan Gasparino <juan.gasparino@gmail.com>
2023-10-05 11:46:08 +0200

    first commit twitter card

---
+++ react/twitter-card/src/App.jsx
@@ -0,0 +1,13 @@
+import { React } from 'react-dom'
+
+import './App.css'
+
+export function App () {
+  return (
+    <article>
+      <header>
+        <img src="" alt="" />
+      </header>
+    </article>
+  )
+}
