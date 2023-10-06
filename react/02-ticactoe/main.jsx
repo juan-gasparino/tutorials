75c37882c1d7f09fc9c17792167cb9f6ab89990d
Juan Gasparino <juan.gasparino@gmail.com>
2023-10-06 07:44:17 +0200

    finish tic-tac-toe

--- react/02-ticactoe/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { App } from './src/App.jsx'
-
-import './styles/App.css'
-
-const root = createRoot(document.getElementById('root'))
-
-root.render(<App />)
