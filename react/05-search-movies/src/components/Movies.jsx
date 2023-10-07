e74e7a681f7ba09f00897b20b84ea65c8963adde
Juan Gasparino <juan.gasparino@gmail.com>
2023-10-07 23:47:53 +0200

    update css

--- react/05-search-movies/src/components/Movies.jsx
+++ react/05-search-movies/src/components/Movies.jsx
@@ -4,10 +4,10 @@ import noFoundMoviesMock from '../mocks/not_found.json'
 
 const ListMovies = ({ movies }) => {
   return (
-    <ul>
+    <ul className='movies'>
       {
         movies.map(movie => (
-          <li key={movie.id}>
+          <li className='movie' key={movie.id}>
             <h3>{movie.title}</h3>
             <p>{movie.year}</p>
             <img src={movie.image} alt={movie.title} />
